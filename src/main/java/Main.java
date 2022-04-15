0e6cd516f56266df6d8b5fb1372e893af7f7f8c3
SEOSLA <realseoyt@gmail.com>
2022-04-15 09:30:15 +0900

    [modify]main branch로 dev_kjs를 rebase

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import EmpManager.EmpManager;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args){
-
-        new EmpManager().start(args);
+        new EmpManager().doProcess(args[0], args[1]);
     }
 }
