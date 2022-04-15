207e7c2840963fb1afb49ec92b03ffd3ace20407
강문식 <kmoons888@gmail.com>
2022-04-15 18:12:20 +0900

    [modify] option2(search 옵션) 처리 할 수 있도록

--- src/main/java/com/firefly/Database/EmployeeDB.java
+++ src/main/java/com/firefly/Database/EmployeeDB.java
@@ -3,6 +3,7 @@ package com.firefly.Database;
 import com.firefly.EmployeeInfo.EmployeeInfo;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class EmployeeDB implements IEmployeeDB {
@@ -22,7 +23,7 @@ public class EmployeeDB implements IEmployeeDB {
 
 
     @Override
-    public int searchEmployeeCnt(String searchCol, String searchValue) {
+    public int searchEmployeeCnt(String searchCol, String searchValue, String option2) {
         int searchCnt = 0;
 
         if(searchCol.equals("employeeNum")){
@@ -32,9 +33,24 @@ public class EmployeeDB implements IEmployeeDB {
             }
         }
         else if(searchCol.equals("name")){
-            for(EmployeeInfo e : empList) {
-                if(e.getName().equals(searchValue) )
-                    searchCnt++;
+            if(option2!= null && option2.equals("f")){
+                for(EmployeeInfo e : empList) {
+                    if(e.getFirstName().equals(searchValue) )
+                        searchCnt++;
+                }
+            }
+            else if(option2!= null &&option2.equals("l")){
+                for(EmployeeInfo e : empList) {
+                    if(e.getLastName().equals(searchValue) )
+                        searchCnt++;
+                }
+
+            }
+            else {
+                for (EmployeeInfo e : empList) {
+                    if (e.getName().equals(searchValue))
+                        searchCnt++;
+                }
             }
 
         }
@@ -46,16 +62,53 @@ public class EmployeeDB implements IEmployeeDB {
 
         }
         else if(searchCol.equals("phoneNum")){
-            for(EmployeeInfo e : empList) {
-                if(e.getPhoneNumByString().equals(searchValue))
-                    searchCnt++;
+            if( "m".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getPhoneNumMid() == Integer.parseInt(searchValue) )
+                        searchCnt++;
+                }
+            }
+            else if("l".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getPhoneNumLast() == Integer.parseInt(searchValue) )
+                        searchCnt++;
+                }
+
+            }
+            else {
+                for (EmployeeInfo e : empList) {
+                    if (e.getPhoneNumByString().equals(searchValue))
+                        searchCnt++;
+                }
             }
 
         }
         else if(searchCol.equals("birthday")){
-            for(EmployeeInfo e : empList) {
-                if(e.getBirthdayByString().equals(searchValue))
-                    searchCnt++;
+            if( "y".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthYear() == Integer.parseInt(searchValue) )
+                        searchCnt++;
+                }
+            }
+            else if("m".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthMonth() == Integer.parseInt(searchValue) )
+                        searchCnt++;
+                }
+
+            }
+            else if("d".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthDayOnly() == Integer.parseInt(searchValue) )
+                        searchCnt++;
+                }
+
+            }
+            else {
+                for (EmployeeInfo e : empList) {
+                    if (e.getBirthdayByString().equals(searchValue))
+                        searchCnt++;
+                }
             }
 
         }
@@ -72,6 +125,117 @@ public class EmployeeDB implements IEmployeeDB {
 
         return searchCnt;
     }
+
+
+
+    @Override
+    public List<EmployeeInfo> searchEmployeeTop5(String searchCol, String searchValue, String option2) {
+        List<EmployeeInfo> emps = new LinkedList<>();
+
+        if(searchCol.equals("employeeNum")){
+            for(EmployeeInfo e : empList) {
+                if(e.getEmployeeNumByString().equals(searchValue) )
+                    emps.add(e);
+            }
+        }
+        else if(searchCol.equals("name")){
+            if( "f".equals(option2) ){
+                for(EmployeeInfo e : empList) {
+                    if(e.getFirstName().equals(searchValue) )
+                        emps.add(e);
+                }
+            }
+            else if( "l".equals(option2) ){
+                for(EmployeeInfo e : empList) {
+                    if(e.getLastName().equals(searchValue) )
+                        emps.add(e);
+                }
+
+            }
+            else{
+                for(EmployeeInfo e : empList) {
+                    if(e.getName().equals(searchValue) )
+                        emps.add(e);
+                }
+
+            }
+
+        }
+        else if(searchCol.equals("cl")){
+            for(EmployeeInfo e : empList) {
+                if(e.getCl().equals(EmployeeInfo.CareerLevel.valueOf(searchValue)))
+                    emps.add(e);
+            }
+
+        }
+        else if(searchCol.equals("phoneNum")){
+            if( "m".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getPhoneNumMid() == Integer.parseInt(searchValue) )
+                        emps.add(e);
+                }
+            }
+            else if("l".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getPhoneNumLast() == Integer.parseInt(searchValue) )
+                        emps.add(e);
+                }
+
+            }
+            else {
+                for (EmployeeInfo e : empList) {
+                    if (e.getPhoneNumByString().equals(searchValue))
+                        emps.add(e);
+                }
+            }
+
+        }
+        else if(searchCol.equals("birthday")){
+            if( "y".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthYear() == Integer.parseInt(searchValue) )
+                        emps.add(e);
+                }
+            }
+            else if("m".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthMonth() == Integer.parseInt(searchValue) )
+                        emps.add(e);
+                }
+
+            }
+            else if("d".equals(option2)){
+                for(EmployeeInfo e : empList) {
+                    if(e.getBirthDayOnly() == Integer.parseInt(searchValue) )
+                        emps.add(e);
+                }
+
+            }
+            else {
+                for (EmployeeInfo e : empList) {
+                    if (e.getBirthdayByString().equals(searchValue))
+                        emps.add(e);
+                }
+            }
+
+        }
+        else if(searchCol.equals("certi")){
+            for(EmployeeInfo e : empList) {
+                if(e.getCerti().equals(EmployeeInfo.Certificate.valueOf(searchValue)))
+                    emps.add(e);
+            }
+
+        }
+        else{
+            return null;
+        }
+
+        emps.sort((EmployeeInfo e1, EmployeeInfo e2) -> e1.getEmployeeNumByString().compareTo( e2.getEmployeeNumByString() ) );
+
+        return emps.size() <= 5 ? emps : emps.subList(0,5);
+    }
+
+
 
 
     @Override
--- src/main/java/com/firefly/Database/IEmployeeDB.java
+++ src/main/java/com/firefly/Database/IEmployeeDB.java
@@ -2,9 +2,12 @@ package com.firefly.Database;
 
 import com.firefly.EmployeeInfo.EmployeeInfo;
 
+import java.util.List;
+
 public interface IEmployeeDB {
     int addEmployee(EmployeeInfo e);
     int delEmployee(String searchCol, String searchValue);
-    int searchEmployeeCnt(String searchCol, String searchValue);
+    int searchEmployeeCnt(String searchCol, String searchValue, String Option2);
+    List<EmployeeInfo> searchEmployeeTop5(String searchCol, String searchValue, String Option2);
     int modEmployee(String searchCol, String searchValue, String modCol, String modValue);
 }
