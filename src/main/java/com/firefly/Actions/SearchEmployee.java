4a3f9bff55f7711c66db33bb0fe89ea2c46a428b
김준식 <yibem@daum.net>
2022-04-18 16:18:46 +0900

    [add] SearchEmployee 기능을 추가하셨습니다. 현재 search기능은 EmployeeDB의 hash table을 사용하지 않고 자체적으로 검색합니다. merge 완료 후 hash검색 반영하겠습니다. 사번을 정렬하는 logic이 포함되어 있습니다.

--- src/main/java/com/firefly/Actions/SearchEmployee.java
+++ src/main/java/com/firefly/Actions/SearchEmployee.java
@@ -1,25 +1,131 @@
 package com.firefly.Actions;
 
+import com.firefly.Database.EmployeeDB;
+import com.firefly.EmployeeInfo.EmployeeInfo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.abs;
 
 public class SearchEmployee implements IActionEmployee {
 
+    //DB전달 방법을 정하지 않은 상태라서 임시로 만들어서 사용하였습니다.
+    //전달 방식 확정 후 삭제
+    ArrayList<EmployeeInfo> empDb = new ArrayList<>();
+
+    //DB전달 방법을 정하지 않은 상태라서 임시로 만들어서 사용하였습니다.
+    //전달 방식 확정 후 삭제
+    public void setEmployeeDb(ArrayList<EmployeeInfo> empDb){
+        this.empDb = empDb;
+    }
 
     @Override
     public List<String> action(HashMap<String, String> inputCommand){
+        List<String> output = new ArrayList<>();
+        String column = inputCommand.get("VALUE1");
+        String keyword = inputCommand.get("VALUE2");
+        String option1 = inputCommand.get("OPTION1");
+        String option2 = inputCommand.get("OPTION2");
+        ArrayList<EmployeeInfo> filtered = new ArrayList<>();
 
-        //실제 실행부분
-        //-p 옵션 있을경우 record를 출력(max5개)
-        if(inputCommand.get("OPTION1").equals("-p")){
-
+        if(column.equals("employeeNum")){
+            int employeeNum = Integer.parseInt(keyword);
+            filtered.add(empDb.stream().filter(emp ->
+                    emp.getEmployeeNumByInt() == employeeNum).findFirst().get());
         }
-        //-p 옵션 없을경우 record 수를 출력
-        else{
-            //System.out.println("SCH,");
+        else if(column.equals("name")) {
+            if(option2.equals("-f")){
+                filtered = empDb.stream().filter(emp ->
+                        emp.getFirstName().equals(keyword)).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else if(option2.equals("-l")){
+                filtered = empDb.stream().filter(emp ->
+                        emp.getLastName().equals(keyword)).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else {
+                filtered = empDb.stream().filter(emp ->
+                        emp.getName().equals(keyword)).collect(Collectors.toCollection(ArrayList::new));
+            }
+        }
+        else if(column.equals("cl")){
+            filtered = empDb.stream().filter(emp ->
+                    emp.getCl().name().equals(keyword)).collect(Collectors.toCollection(ArrayList::new));
+        }
+        else if(column.equals("phoneNum")) {
+            int phoneNum;
+            if(option2.equals("-m")) {
+                phoneNum = Integer.parseInt(keyword.split("-")[1]);
+                filtered = empDb.stream().filter(emp ->
+                        emp.getPhoneNumMid() == phoneNum).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else if(option2.equals("-l")){
+                phoneNum = Integer.parseInt(keyword.split("-")[2]);
+                filtered = empDb.stream().filter(emp ->
+                        emp.getPhoneNumLast() == phoneNum).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else {
+                phoneNum = Integer.parseInt(keyword.split("-")[1] + keyword.split("-")[2]);
+                filtered = empDb.stream().filter(emp ->
+                        emp.getPhoneNum8digitsInt() == phoneNum).collect(Collectors.toCollection(ArrayList::new));
+            }
+        }
+        else if(column.equals("birthday")) {
+            int birthday;
+            if (option2.equals("-y")) {
+                birthday = Integer.parseInt(keyword.substring(0, 4));
+                filtered = empDb.stream().filter(emp ->
+                        emp.getBirthYear() == birthday).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else if (option2.equals("-m")) {
+                birthday = Integer.parseInt(keyword.substring(4, 6));
+                filtered = empDb.stream().filter(emp ->
+                        emp.getBirthMonth() == birthday).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else if (option2.equals("-d")) {
+                birthday = Integer.parseInt(keyword.substring(6, 8));
+                filtered = empDb.stream().filter(emp ->
+                        emp.getBirthDayOnly() == birthday).collect(Collectors.toCollection(ArrayList::new));
+            }
+            else {
+                birthday = Integer.parseInt(keyword);
+                filtered = empDb.stream().filter(emp ->
+                        emp.getBirthdayByInt() == birthday).collect(Collectors.toCollection(ArrayList::new));
+            }
+        }
+        else if(column.equals("certi")){
+            filtered = empDb.stream().filter(emp ->
+                    emp.getCerti().name().equals(keyword)).collect(Collectors.toCollection(ArrayList::new));
+        }
+        else {
+            //input error
         }
 
-        return null;
+        if(filtered.size() == 0){
+            output.add("SCH,NONE");
+            return output;
+        }
+
+        if(option1.equals("-p")) {
+            filtered.stream()
+                    .sorted(Comparator.comparing(EmployeeInfo::getEmployeeNumYear)
+                            .thenComparing(EmployeeInfo::getEmployeeNumExceptYear)).limit(5)
+                    .forEach(emp -> output.add("SCH," +
+                            emp.getEmployeeNumByString() + "," +
+                            emp.getName() + "," +
+                            emp.getCl().name() + "," +
+                            emp.getPhoneNumByString() + "," +
+                            emp.getBirthdayByString() + "," +
+                            emp.getCerti().name()));
+        }
+        else {
+            output.add("SCH," + filtered.size());
+        }
+
+        return output;
     }
 
     @Override
--- src/test/java/com/firefly/Actions/SearchEmployeeTest.java
+++ src/test/java/com/firefly/Actions/SearchEmployeeTest.java
@@ -1,7 +1,144 @@
 package com.firefly.Actions;
 
+import com.firefly.Database.EmployeeDB;
+import com.firefly.Database.IEmployeeDB;
+import com.firefly.EmployeeInfo.EmployeeInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchEmployeeTest {
 
+    EmployeeInfo info1, info2, info3, info4, info5, info6, info7, info8, info9, info10, info11, info12, info13, info14, info15;
+
+    IEmployeeDB db = new EmployeeDB();
+    ArrayList<EmployeeInfo> empDb = new ArrayList<>();
+
+    @BeforeEach
+    void setup(){
+        info1 = new EmployeeInfo("10000001", "BOBBY KIM", "CL2",
+                "010-3333-7777", "19850715", "PRO");
+        info2 = new EmployeeInfo("10000002", "NA LEE", "CL2",
+                "010-1111-1321", "19770315", "PRO");
+        info3 = new EmployeeInfo("10000003", "ALL JI", "CL3",
+                "010-3333-1231", "19990115", "PRO");
+        info4 = new EmployeeInfo("10000004", "GILJA HA", "CL3",
+                "010-4444-5555", "19910215", "PRO");
+        info5 = new EmployeeInfo("10000005", "GILDONG HONG", "CL2",
+                "010-3333-4444", "19900115", "PRO");
+        info6 = new EmployeeInfo("10000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info7 = new EmployeeInfo("690000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info8 = new EmployeeInfo("75000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info9 = new EmployeeInfo("00000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info10 = new EmployeeInfo("20000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info11 = new EmployeeInfo("11000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info12 = new EmployeeInfo("01000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info13 = new EmployeeInfo("70000006", "PIO KIM", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info14 = new EmployeeInfo("01000012", "PIO PARK", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+        info15 = new EmployeeInfo("70000011", "PIO PARK", "CL4",
+                "010-2000-1321", "19880715", "PRO");
+
+        empDb.add(info1);
+        empDb.add(info2);
+        empDb.add(info3);
+        empDb.add(info4);
+        empDb.add(info5);
+        empDb.add(info6);
+        empDb.add(info7);
+        empDb.add(info8);
+        empDb.add(info9);
+        empDb.add(info10);
+        empDb.add(info11);
+        empDb.add(info12);
+        empDb.add(info13);
+        empDb.add(info14);
+        empDb.add(info15);
+    }
+
+    @Test
+    void searchTestSorting(){
+        SearchEmployee schTester = new SearchEmployee();
+        schTester.setEmployeeDb(empDb);
+
+        HashMap<String, String> inputValue = new HashMap<>();
+        inputValue.put("COMMAND","SCH");
+        inputValue.put("OPTION1","-p");
+        inputValue.put("OPTION2","-f");
+        inputValue.put("OPTION3","");
+        inputValue.put("VALUE1","name");
+        inputValue.put("VALUE2","PIO");
+
+        List<String> result = schTester.action(inputValue);
+
+        Assertions.assertEquals(5, result.size());
+    }
+
+    @Test
+    void searchTestUpToFive(){
+        SearchEmployee schTester = new SearchEmployee();
+        schTester.setEmployeeDb(empDb);
+
+        HashMap<String, String> inputValue = new HashMap<>();
+        inputValue.put("COMMAND","SCH");
+        inputValue.put("OPTION1","-p");
+        inputValue.put("OPTION2","-l");
+        inputValue.put("OPTION3","");
+        inputValue.put("VALUE1","name");
+        inputValue.put("VALUE2","KIM");
+
+        List<String> result = schTester.action(inputValue);
+
+        Assertions.assertEquals(5, result.size());
+    }
+
+    @Test
+    void searchTestFoundNum(){
+        SearchEmployee schTester = new SearchEmployee();
+        schTester.setEmployeeDb(empDb);
+
+        HashMap<String, String> inputValue = new HashMap<>();
+        inputValue.put("COMMAND","SCH");
+        inputValue.put("OPTION1","");
+        inputValue.put("OPTION2","-l");
+        inputValue.put("OPTION3","");
+        inputValue.put("VALUE1","name");
+        inputValue.put("VALUE2","KIM");
+
+        List<String> result = schTester.action(inputValue);
+
+        Assertions.assertEquals(1, result.size());
+    }
+
+    @Test
+    void searchTestByEmployeeNum(){
+        SearchEmployee schTester = new SearchEmployee();
+        schTester.setEmployeeDb(empDb);
+
+        HashMap<String, String> inputValue = new HashMap<>();
+        inputValue.put("COMMAND","SCH");
+        inputValue.put("OPTION1","-p");
+        inputValue.put("OPTION2","-l");
+        inputValue.put("OPTION3","");
+        inputValue.put("VALUE1","employeeNum");
+        inputValue.put("VALUE2","01000012");
+
+        List<String> result = schTester.action(inputValue);
+
+        Assertions.assertEquals(1, result.size());
+    }
 }
